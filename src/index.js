9f8188beb3a5649ad19c45895d7f532c703e1329
Lenny Huynh <lennyhuynh526@gmail.com>
2023-06-28 21:30:29 -0700

    msg

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import requestIp from 'request-ip';
+import IP2Location from 'ip2location-nodejs';
+
+let ip2location = new IP2Location("IP2LOCATION-LITE-DB1.BIN");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,9 +11,11 @@ app.use(requestIp.mw());
 
 app.get('/ip', (req, res) => {
     let clientIp = requestIp.getClientIp(req);
+    let geolocation = ip2location.getAll(clientIp);
 
     const response = {
-        ip: clientIp
+        ip: clientIp,
+        isInUS: geolocation.country_short === "US" ? "yes" : "no"
     };
 
     res.json(response);
@@ -19,4 +24,3 @@ app.get('/ip', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
